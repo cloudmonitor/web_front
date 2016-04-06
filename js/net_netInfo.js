aeb8bcee89331317bdca0ba036db13b9854fd9d8
269117220 <269117220@qq.com>
2016-04-06 14:42:30 +0800

    start network

--- js/net_netInfo.js
+++ js/net_netInfo.js
@@ -64,6 +64,12 @@ $(function() {
 function setList(data, i, status) {
     var str = "<tbody><tr><td><input type='checkbox'></td><td><a href='#/net/net-desc?" + i + "&" + status + "'>" + data.name + "</a></td><td>" + data.subnets + "</td><td>" + data.shared + "</td>" +
         "<td>" + data.status + "</td><td>" + data.admin_state_up +
-        "</td><td><div class='btn-group'><button type='button' class='btn btn-default btn-sm'>" + "创建快照" + "</button><button type='button' class='btn btn-default btn-sm dropdown-toggle' data-toggle='dropdown'><span class='caret'></span><span class='sr-only'>" + "切换下拉菜单" + "</span></button><ul class='dropdown-menu' role='menu'><li><a href='#'>" + "功能" + "</a></li><li><a href='#'>" + "另一个功能" + "</a></li><li><a href='#'>" + "其他" + "</a></li></ul></div></td></tr></tbody>";
+        "</td><td><div class='btn-group'>"+
+        "<button type='button' class='btn btn-default btn-sm'>" + "编辑网络" + "</button>"+
+        "<button type='button' class='btn btn-default btn-sm dropdown-toggle' data-toggle='dropdown'><span class='caret'></span><span class='sr-only'>" + "切换下拉菜单" + "</span></button>"+
+        "<ul class='dropdown-menu' role='menu'>"+
+        "<li><a href='#'>" + "增加子网" + "</a></li>"+
+        "<li><a href='#'>" + "删除网络" + "</a></li>"+
+        "</ul></div></td></tr></tbody>";
     $(".instance_info").append(str);
 }
