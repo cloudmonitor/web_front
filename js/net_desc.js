8741977f7167fb1204b94512802c4338f6429978
269117220 <269117220@qq.com>
2016-04-07 20:25:42 +0800

    subnet_delete ok

--- js/net_desc.js
+++ js/net_desc.js
@@ -190,8 +190,45 @@ $(function() {
 
 });
 
+//----------------删除网络
+//---批量删
+$(".sub_checks").change(function() {
+    if ($(".sub_checks:checked").length != 0) {
+        $(".delete_subnetInfo").attr("disabled", false);
+    } else {
+        $(".delete_subnetInfo").attr("disabled", true);
+    }
+});
+$(".delete_subnetInfo").click(function() {
+    var subnets = { "subnet_ids": [] };
+    var i = 0;
+    $(".sub_checks:checked").each(function() {
+        subnets['subnet_ids'][i++] = this.id;
+    });
+    deletesubnetAjax(subnets);
+});
+//-------单删
+$(document).on("click", ".delete_subnetSimple", function() {
+    var subnets = { "subnet_ids": [] };
+    subnets['subnet_ids'][0] = this.id;
+    deletesubnetAjax(subnets);
+});
+
+function deletesubnetAjax(subnets) {
+    $.ajax({
+        type: "POST",
+        data: JSON.stringify(subnets),
+        contentType: "application/json",
+        url: config['host'] + "/subnet/delete?token=" + window.localStorage.token,
+        success: function(data) {
+            window.location.href = "#/net/net";
+        }
+    });
+}
+//----------------
 function setNetInfo(netInfo, manager_status) {
     //--------网络概况
+    $("#project_name").text(netInfo.name);
     $("#net_name").html(netInfo.name);
     $("#net_id").html(netInfo.id);
     $("#net_proId").html(netInfo.tenant_id);
@@ -207,9 +244,158 @@ function setNetInfo(netInfo, manager_status) {
     else
         $("#net_mtu").html(netInfo.mtu);
 }
+//---------------创建子网面板配置
+var sub1_flag = true;
+$(".createchoose_subnet").click(function() {
+    $(".createsubnet_multi").slideToggle();
+    if (sub1_flag) {
+        $(".info_pic2").removeClass("fa fa-angle-double-down");
+        $(".info_pic2").addClass("fa fa-angle-double-up");
+        sub1_flag = false;
+    } else {
+        $(".info_pic2").removeClass("fa fa-angle-double-up");
+        $(".info_pic2").addClass("fa fa-angle-double-down");
+        sub1_flag = true;
+    }
+});
+var sub2_flag = true;
+$(".showmoresubnetInfo").click(function() {
+    $(".showMoreInfo").slideToggle();
+    if (sub2_flag) {
+        $(".info_pic3").removeClass("fa fa-angle-double-down");
+        $(".info_pic3").addClass("fa fa-angle-double-up");
+        sub2_flag = false;
+    } else {
+        $(".info_pic3").removeClass("fa fa-angle-double-up");
+        $(".info_pic3").addClass("fa fa-angle-double-down");
+        sub2_flag = true;
+    }
+});
+$(".subnet_opengateway").change(function() {
+    if (!$(".subnet_opengateway").is(":checked"))
+        $(".creategateway_adrr").attr("disabled", true);
+    else
+        $(".creategateway_adrr").attr("disabled", false);
+});
+$(document).on("click", ".add_subnetInfo", function() {
+    $(".private_selected").empty();
+    $(".private_selected").append('<option value="test">选择私有网络</option>');
+    var servers = JSON.parse(localStorage.net_tempInfo)['networks'];
+    var server;
+    var str = "";
+    for (var i = 0; i < servers.length; i++) {
+        server = servers[i];
+        str += '<option value="' + server.id + '">' + server.name + '</option>';
+    }
+    $(".private_selected").append(str);
+});
+//---------创建子网
+$(".create_subnetworkOk").click(function() {
+    //------子网名称
+    var sub_name = $(".createsubnet_name").val();
+    //------私有网络ID
+    var private_net = $(".private_selected").val();
+    //---获取子网的地址
+    var object = $(".createradio_subnetaddr:checked");
+    var num_1 = object.parent().siblings().eq(0).val();
+    var num_2 = object.parent().siblings().eq(2).val();
+    var num_3 = object.parent().siblings().eq(4).val();
+    var num_4 = object.parent().siblings().eq(6).val();
+    var num_5 = object.parent().siblings().eq(8).val();
+    var str_ip = num_1 + "." + num_2 + "." + num_3 + "." + num_4 + "/" + num_5;
+    //------------开启网关判断
+    var gateWay;
+    if ($(".subnet_opengateway").prop("checked")) {
+        gateWay = $(".creategateway_adrr").val();
+    }
+    //--------DHCP
+    var dhcp;
+    if ($(".subnet_openDHCP").prop("checked")) {
+        dhcp = true;
+    } else {
+        dhcp = false;
+    }
+
+    //--------数据提交
+    var subnet = {
+        "subnet": {
+            "name": "",
+            "network_id": "",
+            "ip_version": 4,
+            "cidr": "10.0.0.1",
+            "gateway_ip": "",
+            "enable_dhcp": false,
+            "allocation_pools": [],
+            "dns_nameservers": []
+        }
+    }
+    var subnetInfo = subnet['subnet'];
+    if (sub_name == "") {
+        alert("子网名称必填！");
+        return;
+    }
+    subnetInfo.name = sub_name;
+    if (private_net == "test") {
+        alert("请选择私有网络！");
+        return;
+    }
+    subnetInfo.network_id = private_net;
+    subnetInfo.cidr = str_ip;
+    if (gateWay == "")
+        delete subnetInfo.gateway_ip;
+    else
+        subnetInfo.gateway_ip = gateWay;
+    subnetInfo.enable_dhcp = dhcp;
+    //--------地址池
+    if ($(".Addrpool").val() != "") {
+        var arr = $(".Addrpool").val().split("\n");
+
+        var temp_arr = [];
+        var temp;
+        var object = { "start": "", "end": "" };
+        for (var i = 0; i < arr.length; i++) {
+            temp = arr[i].split(",");
+            if (temp.length % 2 != 0) {
+                alert("地址池起始地址有误！");
+                return;
+            }
+            object.start = temp[0];
+            object.end = temp[1];
+            temp_arr[i] = object;
+        }
+        subnetInfo.allocation_pools = temp_arr;
+    }
+    //--------DNS服务
+    if ($(".DNSserver").val() != "") {
+        var arr = $(".DNSserver").val().split("\n");
+        for (var i = 0; i < arr.length; i++) {
+            subnetInfo.dns_nameservers[i] = arr[i];
+        }
+    }
+    createSubnetAJAX(subnet);
+});
+
+function createSubnetAJAX(subnet) {
+    console.log(JSON.stringify(subnet));
+    $.ajax({
+        type: "POST",
+        data: JSON.stringify(subnet),
+        contentType: "application/json",
+        url: config['host'] + "/subnet/create?token=" + window.localStorage.token,
+        success: function(data) {
+            if (JSON.parse(data)['subnet'] == null || JSON.parse(data)['subnet'] == "undefined") {
+                console.log(data);
+                alert("请检查子网配置格式！");
+            } else {
+                window.location.reload();
+                window.location.href = "#/net/net";
+            }
+        }
+    });
+}
 
 function set_subNet(data, i) {
-    var str = '<tbody><tr><td><input type="checkbox"></td>' +
+    var str = '<tbody><tr><td><input type="checkbox" class="sub_checks" id="' + data.id + '"></td>' +
         '<td><a href="#/net/subnet-desc?' + i + '">' + data.name + '</a>' +
         '</td><td>' + data.cidr +
         '</td><td>' + 'ipv' + data.ip_version +
@@ -219,9 +405,7 @@ function set_subNet(data, i) {
         '<span class="sr-only">切换下拉菜单</span>' +
         '</button>' +
         '<ul class="dropdown-menu" role="menu">' +
-        '<li><a href="#">功能</a></li>' +
-        '<li><a href="#">另一个功能</a></li>' +
-        '<li><a href="#">其他</a></li>' +
+        '<li id="' + data.id + '" class="delete_subnetSimple"><a href="javascript:void(0)"><font color="red">删除子网</font></a></li>' +
         '</ul>' +
         '</div>' +
         '</td>' +
