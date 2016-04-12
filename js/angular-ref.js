1d2304769c882be009e648026559117d7576ce64
wuhonglei <1017368065@qq.com>
2016-04-12 15:03:38 +0800

    增加了密钥页面的js

--- js/angular-ref.js
+++ js/angular-ref.js
@@ -23,6 +23,11 @@ myApp.config(function($routeProvider) {
             templateUrl: 'pages/compute/instance_desc.html',
             controller: 'instanceDesc'
         })
+        // 计算 -- 密钥对
+        .when('/compute/key-pair', {
+            templateUrl: 'pages/compute/key-pair.html',
+            controller: 'keyPairCtrl'
+        })
         // 网络 -- 拓扑
         .when('/net/topology', {
             templateUrl: 'pages/net/topology.html',
@@ -168,7 +173,8 @@ myApp.controller('portnetDesc', function($scope) {
     $scope.$parent.loadScript('js/net_desc_portDesc.js', 'text/javascript', 'utf-8');
     $("head title").text("网络");
 });
-
+// 计算 -- 密钥对
+myApp.controller('keyPairCtrl', keyPairCtrl);
 // 网络与安全 -- 安全组
 myApp.controller('secGroupCtrl', function($scope) {
     $scope.$parent.loadScript('js/config.js', 'text/javascript', 'utf-8');
@@ -268,6 +274,7 @@ function myCtrl($scope) {
     };
 }
 
+// 浮动IP控制器
 function floatingIPCtrl($scope, $http) {
     $("head title").text("浮动IP");
     // ip 数目
@@ -727,4 +734,220 @@ function floatingIPCtrl($scope, $http) {
             });
         };
     };
+}
+
+// ===============================计算  密钥=====================================//
+// 密钥对控制器
+function keyPairCtrl($scope, $http) {
+    // 初始化值
+    // 默认禁用删除按钮
+    $scope.delBtnDis = true;
+
+    // 获取密钥对
+    $scope.getKeyItems = function() {
+        var url = config.host + "/keypairs?token=" + window.localStorage.token;
+        $http.get(url).then(function(response) {
+            // 请求成功
+            var data = response.data;
+            console.info("返回的数据：", data);
+            $scope.keyItems = data.keypairs;
+            $scope.itemCount = data.keypairs.length;
+        }, function(response) {
+            // 请求失败
+            $scope.itemCount = 0;
+            createAndHideAlert({
+                "message": "密钥信息请求失败",
+                "className": "alert-danger"
+            });
+        });
+    }();
+
+    // 勾选全选按钮
+    $scope.chooseAllBtn = function() {
+        console.info("all = ", $scope.all);
+        if ($scope.itemCount) {
+            // 如果条目数大于0
+            $scope.delBtnDis = ($scope.all) ? false : true;
+        }
+        $scope.one = ($scope.all) ? true : false;
+        console.info("$scope.one = ", $scope.one);
+    };
+
+    // 勾选表格中的列表项目
+    $scope.itemChecked = function(event) {
+        var length = $("td input[type='checkbox']:checked").length;
+        console.info("该项目的索引值: ", $(event.target));
+        if (length) {
+            // 勾选数目大于0
+            console.info("勾选数目 = ", length);
+            $scope.delBtnDis = false;
+            $scope.all = (length == $scope.itemCount) ? true : false;
+            console.info("$scope.all = ", $scope.all);
+        }
+    };
+
+    // 删除密钥对
+    $scope.delKeyBtn = function() {
+        // 分别存放将被删除的密钥及密钥下标
+        var deleteItems = [];
+        var deleteIndex = [];
+        var checkedItems = $("td input[type='checkbox']:checked");
+        var keyItems = $scope.keyItems;
+        for (var i = 0, len = checkedItems.length; i < len; i++) {
+            deleteIndex.push($(checkedItems[i]).attr('index'));
+            deleteItems.push(keyItems[deleteIndex[i]].keypair.name);
+        }
+        $scope.deleteItems = deleteItems;
+        console.info("将被删除的key: ", deleteItems);
+        // 删除key请求(POST)
+        var url = config.host + "/keypairs/delete?token=" + window.localStorage.token;
+        var req = {
+            method: 'POST',
+            url: url,
+            headers: {
+                'Content-Type': "application/json",
+            },
+            data: JSON.stringify({ "keypair_names": deleteItems })
+        };
+        // 点击确认删除按钮
+        $scope.confirmeDel = function() {
+            console.info("req.data : ", deleteItems);
+            $http(req).then(function(response) {
+                // POST请求成功  {"name1": 202, "name2": 202}
+                console.info("删除后返回的数据: ", response.data);
+                var data = response.data;
+                var SUCCESS = 202;
+                var INCREASE = 0;
+                for (var i = 0, len = checkedItems.length; i < len; i++) {
+                    var name = deleteItems[i];
+                    if (data[name] === SUCCESS) {
+                        // 删除成功
+                        createAndHideAlert({
+                            "message": "<strong>密钥删除成功:</strong><br>" + deleteItems[i],
+                            "className": "alert-success"
+                        });
+                        // 从列表中删除这个元素
+                        var index = deleteIndex[i] - INCREASE;
+                        INCREASE = INCREASE + 1;
+                        keyItems.splice(index, 1);
+                    } else {
+                        // 设置状态为未选中
+                        // checkedItems[i].checked = false;
+                        createAndHideAlert({
+                            "message": "<strong>密钥删除失败:</strong><br>" + deleteItems[i],
+                            "className": "alert-danger"
+                        });
+                    }
+                }
+                // 隐藏模态框
+                $("#deleteKey-modal").modal('hide');
+                console.info("未删除的条目: ", keyItems);
+                $scope.keyItems = keyItems;
+                $scope.all = false;
+                $scope.delBtnDis = true;
+                $scope.itemCount = keyItems.length;
+            }, function(response) {
+                // POST请求失败
+                createAndHideAlert({
+                    "message": "请求删除失败",
+                    "className": "alert-danger"
+                });
+            });
+        };
+    };
+
+
+    // 创建密钥模态框按钮
+    $scope.createKeyModal = function() {
+        $scope.keyName = null;
+
+        // 自动下载本地文件
+        function download(filename, text) {
+            var element = document.createElement('a');
+            element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+            element.setAttribute('download', filename);
+
+            element.style.display = 'none';
+            document.body.appendChild(element);
+
+            element.click();
+
+            document.body.removeChild(element);
+        }
+
+        // 创建密钥 -- 方式一
+        $scope.createKey = function() {
+            var name = $scope.keyName;
+            var url = config.host + "/keypairs/create?token=" + window.localStorage.token;
+            var req = {
+                method: 'POST',
+                url: url,
+                headers: {
+                    'Content-Type': "application/json",
+                },
+                data: JSON.stringify({ "keypair": { "name": name } })
+            };
+            $http(req).then(function(response) {
+                // POST请求成功
+                var data = response.data;
+                console.info("请求成功返回的数据： ", data);
+                // 删除Object中的private_key、user_id属性
+                $scope.keyItems.push(data);
+                createAndHideAlert({
+                    "message": "密钥创建成功:<br>" + data.keypair.name,
+                    "className": "alert-success"
+                });
+                $scope.itemCount = $scope.keyItems.length;
+                $("#createKey-modal").modal('hide');
+                // parameters1: 文件名   parameter2: 文件内容
+                var filename =  data.keypair.name + ".pem";
+                var fileContent = data.keypair.private_key;
+                download(filename, fileContent);
+            }, function(response) {
+                // POST请求失败
+                createAndHideAlert({
+                    "message": "请求创建失败",
+                    "className": "alert-danger"
+                });
+            });
+        };
+
+        // 创建密钥 -- 方式二
+        $scope.createByImportKey = function() {
+            var name = $scope.keyName;
+            var public_key = $scope.public_key;
+            var url = config.host + "/keypairs/create?token=" + window.localStorage.token;
+            var req = {
+                method: 'POST',
+                url: url,
+                headers: {
+                    'Content-Type': "application/json",
+                },
+                data: JSON.stringify({ "keypair": { "name": name, "public_key": public_key } })
+            };
+            $http(req).then(function(response) {
+                var data = response.data;
+                console.info("请求成功返回的数据： ", data);
+                // 删除Object中的private_key、user_id属性
+                data.keypair.private_key = undefined;
+                data.keypair.user_id = undefined;
+                $scope.keyItems.push(data);
+                createAndHideAlert({
+                    "message": "密钥创建成功:<br>" + data.keypair.name,
+                    "className": "alert-success"
+                });
+                $scope.itemCount = $scope.keyItems.length;
+                $("#createKey-modal").modal('hide');
+
+            }, function(response) {
+                // POST请求失败
+                createAndHideAlert({
+                    "message": "请求创建失败",
+                    "className": "alert-danger"
+                });
+            });
+        }
+    };
+
+
 }
